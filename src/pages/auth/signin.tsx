ee16a307fcd5c114a8c0d2a8b13b442d6b39ac6f
connorjbarry <barry31@purdue.edu>
2023-03-11 19:29:08 -0600

    fixed all linting errors

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,57 +1,80 @@
-import React from "react";
-import { getProviders, signIn } from "next-auth/react";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React, { type ReactNode } from "react";
+import {
+  type ClientSafeProvider,
+  getProviders,
+  type LiteralUnion,
+  signIn,
+} from "next-auth/react";
 import { BsGoogle, BsGithub } from "react-icons/bs";
 import { FaDiscord } from "react-icons/fa";
+import { type BuiltInProviderType } from "next-auth/providers";
+interface ProviderStylesType {
+  bgcolor: string;
+  iconcolor: string;
+  icon: ReactNode;
+}
 
-const providerStyles = [
-    {
-      bgcolor: "bg-[#7289DA]",
-      iconcolor: "text-gray-100",
-      icon: <FaDiscord />,
-    },
-    {
-      bgcolor: "bg-gray-800",
-      iconcolor: "text-gray-100",
-      icon: <BsGithub />,
-    },
-    {
-      bgcolor: "bg-gray-100",
-      iconcolor: "text-gray-800",
-      icon: <BsGoogle />,
-    },
+const providerStyles: ProviderStylesType[] = [
+  {
+    bgcolor: "bg-[#7289DA]",
+    iconcolor: "text-gray-100",
+    icon: <FaDiscord />,
+  },
+  {
+    bgcolor: "bg-gray-800",
+    iconcolor: "text-gray-100",
+    icon: <BsGithub />,
+  },
+  {
+    bgcolor: "bg-gray-100",
+    iconcolor: "text-gray-800",
+    icon: <BsGoogle />,
+  },
 ];
 
-export default function SignIn(providers: any) {
-  const providerList = Object.values(providers.providers).map(
-    (provider: any, index: number) => {
-      return (
-        <div
-          key={provider.name}
-          className="flex flex-row justify-center items-center text-lg sm:text-xl 2xl:text-2xl w-full py-4"
+export default function SignIn(
+  providers: Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null
+) {
+  if (!providers) return null;
+  const providerList = Object.values(
+    providers.providers as unknown as string[]
+  ).map((provider: any, index: number) => {
+    return (
+      <div
+        key={provider.id}
+        id={provider.name}
+        className="flex w-full flex-row items-center justify-center py-4 text-lg sm:text-xl 2xl:text-2xl"
+      >
+        <button
+          onClick={() =>
+            void signIn(provider.id, {
+              callbackUrl: `${window.location.origin}/protected`,
+            })
+          }
+          className={`flex w-2/3 flex-row items-center justify-evenly rounded-2xl transition duration-150 hover:shadow-lg hover:shadow-slate-500/50 ${
+            providerStyles[index]?.bgcolor as string
+          } ${providerStyles[index]?.iconcolor as string}`}
         >
-          <button
-            onClick={() =>
-              signIn(provider.id, {
-                callbackUrl: `${window.location.origin}/protected`,
-              })
-            }
-            className={`flex flex-row justify-evenly items-center rounded-2xl w-2/3 hover:shadow-slate-500/50 hover:shadow-lg transition duration-150 ${providerStyles[index].bgcolor} ${providerStyles[index].iconcolor}`}
-          >
-            <span className="p-1 sm:p-2 lg:p-4">
-              {providerStyles[index]!.icon}
-            </span>
-            <span className="py-1 pr-1 sm:py-2 sm:pr-2 lg:py-4 lg:pr-4 overflow-clip">
-              {provider.name}
-            </span>
-          </button>
-        </div>
-      );
-    }
-  );
+          <span className="p-1 sm:p-2 lg:p-4">
+            {providerStyles[index]?.icon}
+          </span>
+          <span className="overflow-clip py-1 pr-1 sm:py-2 sm:pr-2 lg:py-4 lg:pr-4">
+            {provider.name}
+          </span>
+        </button>
+      </div>
+    );
+  });
 
   return (
-    <div className="flex flex-row justify-center items-center h-screen w-screen bg-slate-900">
-      <div className="flex flex-col justify-center items-center text-center w-1/2 sm:w-1/3 2xl:w-1/5 rounded-lg bg-slate-200">
+    <div className="flex h-screen w-screen flex-row items-center justify-center bg-slate-900">
+      <div className="flex w-1/2 flex-col items-center justify-center rounded-lg bg-slate-200 text-center sm:w-1/3 2xl:w-1/5">
         {providerList}
       </div>
     </div>
--- src/pages/protected/index.tsx
+++ src/pages/protected/index.tsx
@@ -1,36 +1,38 @@
-import React from 'react';
-import { useSession } from 'next-auth/react';
+import React from "react";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 
 const ProtectedPage = (): JSX.Element => {
-    const router = useRouter();
-    const { data: session, status } = useSession({
-      required: true,
-      onUnauthenticated() {
-        // The user is not authenticated, handle it here.
-        router.push("/");
-      },
-    });
+  const router = useRouter();
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      void router.push("/");
+    },
+  });
 
-    const user = session?.user?.name;
+  const user = session?.user?.name;
 
-    return (
-        <>
-            <Head>
-                <title>Protected Page</title>
-                <meta name="description" content="Trustwothy modules team 9 ece461" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className="flex flex-col items-center justify-center min-h-screen">
-                <h1 className="text-8xl">Welcome!</h1>
-                <h2 className="text-6xl">{user}</h2>
-                <p className="text-5xl">Trustworthy modules team 9 ece 461</p>
-                <button onClick={() => signOut()} className="bg-slate rounded-md">Sign out</button>
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Protected Page</title>
+        <meta name="description" content="Trustwothy modules team 9 ece461" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-8xl">Welcome!</h1>
+        <h2 className="text-6xl">{user}</h2>
+        <p className="text-5xl">Trustworthy modules team 9 ece 461</p>
+        <button onClick={() => void signOut()} className="bg-slate rounded-md">
+          Sign out
+        </button>
+      </main>
+    </>
+  );
+};
 
 export default ProtectedPage;
