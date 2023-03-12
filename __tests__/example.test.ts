6fb81e1a99c79190167cf18ee1630db8787b81db
connorjbarry <barry31@purdue.edu>
2023-03-12 09:26:23 -0500

    fixed example test fail

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -4,7 +4,7 @@ describe("Example test", () => {
   it("should pass", () => {
     expect(true).toBe(true);
   });
-  it("should fail", () => {
-    expect(true).toBe(false);
-  });
+  // it("should fail", () => {
+  //   expect(true).toBe(false);
+  // });
 });
