84c375909d630219ae223da2ff7cca71319fdfdb
connorjbarry <barry31@purdue.edu>
2023-03-13 11:39:57 -0500

    removed snapshot, updated test example

--- __tests__/example.test.ts
+++ __tests__/example.test.ts
@@ -1,10 +1,10 @@
 import "@jest/globals";
 
 describe("Example test", () => {
-  it("should pass", () => {
+  it("should pass with true == true", () => {
     expect(true).toBe(true);
   });
-  // it("should fail", () => {
-  //   expect(true).toBe(false);
-  // });
+  it("should pass with true != false", () => {
+    expect(true).not.toBe(false);
+  });
 });
