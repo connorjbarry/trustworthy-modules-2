c7a93ca55c38083aba9b0653e31a0505f40cdd38
hays13 <54752023+hays13@users.noreply.github.com>
2023-03-12 22:31:48 -0400

    Update metric_dependencies.ts
    able to install and start to test metric, but unable to compile at the current point in time

--- metrics/metric_dependencies.ts
+++ metrics/metric_dependencies.ts
@@ -1,16 +1,26 @@
 import axios from 'axios';
-import * as npm from 'npm';
+import { Buffer } from 'buffer';
+
+declare class Promise<T> {
+    constructor(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void);
+    then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null | undefined, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null | undefined): Promise<TResult1 | TResult2>;
+    catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | null | undefined): Promise<T | TResult>;
+    finally(onfinally?: (() => void) | null | undefined): Promise<T>;
+}
 
 interface Dependency {
     name: string;
     version: string;
 }
 
-async function getDependencies(repo_owner: string, GH_token: string): Promise<number> {
+async function getDependencies(repoUrl: string, GH_token: string): Promise<number> {
     const headers = {
         Authorization: `token ${GH_token}`
     };
-    const response = await axios.get(`https://api.github.com/repos/${repo_owner}/contents/package.json`, { headers });
+    const url = new URL(repoUrl);
+    const [repo_owner, repo_name] = url.pathname.split('/').filter(Boolean);
+
+    const response = await axios.get(`https://api.github.com/repos/${repo_owner}/${repo_name}/contents/package.json`, { headers });
     const packageJson = JSON.parse(Buffer.from(response.data.content, 'base64').toString());
     const dependencies = { ...packageJson.dependencies, ...packageJson.devDependencies };
     const currentVersion = packageJson.version;
@@ -18,31 +28,26 @@ async function getDependencies(repo_owner: string, GH_token: string): Promise<nu
     if (Object.keys(dependencies).length === 0) {
         return 1.0;
     }
-    // loads the npm registry metadata and prepares npm for usage within the Node.js environment
-    await new Promise((resolve, reject) => {
-        npm.load({}, (err) => {
-            if (err) reject(err);
-            resolve();
-        });
-    });
 
-    const fetchVersion = (name: string) => new Promise<string>((resolve, reject) => {
-        npm.commands.view([name, 'version'], true, (err, data) => {
-            if (err) reject(err);
-            const versions = Object.keys(data);
-            const latestVersion = versions[versions.length - 1];
-            resolve(latestVersion);
-        });
-    });
+    const fetchVersion = async (name: string) => {
+        const response = await axios.get(`https://registry.npmjs.org/${name}/latest`);
+        return response.data.version;
+    };
 
-    const dependenciesWithVersions = await Promise.all(
-        Object.entries(dependencies).map(async ([name, version]) => ({
+    const promises = Object.entries(dependencies).map(async ([name, version]) => {
+        const depVersion = await fetchVersion(name);
+        return {
             name,
-            version: await fetchVersion(name),
-        }))
-    );
+            version: depVersion
+        }
+    });
 
-    const matchingDependencies = dependenciesWithVersions.filter((dep) => isPinnedToVersion(dep, currentVersion));
+    const dependenciesWithVersions = await Promise.allSettled(promises);
+
+    const matchingDependencies = dependenciesWithVersions.filter((dep) => dep.status === 'fulfilled')
+        .map((dep) => dep.value)
+        .filter((dep) => isPinnedToVersion(dep, currentVersion));
+
     return matchingDependencies.length / dependenciesWithVersions.length;
 }
 
@@ -51,11 +56,12 @@ function isPinnedToVersion(dependency: Dependency, currentVersion: string): bool
     const [depMajor, depMinor] = dependency.version.split('.');
     return depMajor === major && depMinor === minor;
 }
-const repo_owner = 'repo_owner';
+
+const repoUrl = 'https://github.com/nullivex/nodist';
 const GH_token = 'TOKEN';
-getDependencies(repo_owner, GH_token).then((percentage) => {
+getDependencies(repoUrl, GH_token).then((percentage) => {
     const decimalScore = percentage.toFixed(2);
-    console.log(`Percentage of dependencies pinned to current version: ${decimalScore}`);
+    console.log(`Decimal of dependencies pinned to current version: ${decimalScore}`);
 }).catch((err) => {
     console.error(err);
 });
