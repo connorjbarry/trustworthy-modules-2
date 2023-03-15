dbc8ec4db4020d8c9e893cd9b3a96b3103520575
connorjbarry <barry31@purdue.edu>
2023-03-15 09:40:39 -0500

    updated navbar testing

--- __tests__/navbar.test.tsx
+++ __tests__/navbar.test.tsx
@@ -3,6 +3,8 @@ import NavBar from "../src/components/NavBar";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+// TODO: Add tests for NavBar component
+
 describe("NavBar testing", () => {
   it("should render the NavBar component", () => {
     render(<NavBar />);
@@ -15,7 +17,9 @@ describe("NavBar testing", () => {
   it("should have an image that links to homepage", () => {
     render(<NavBar />);
     // the parent element should have attribute href="/"
-    expect(screen.getByText("Image")).toHaveAttribute("href", "/");
+    expect(
+      screen.getByText("Trustworthy Modules Registry").parentElement
+    ).toHaveAttribute("href", "/");
   });
   it("should go to account page when account link is clicked", () => {
     render(<NavBar />);
