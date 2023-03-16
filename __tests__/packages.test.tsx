7241847a641ee339f519bd917c592c0221954723
connorjbarry <barry31@purdue.edu>
2023-03-16 18:18:25 -0500

    state test is not passing

--- __tests__/packages.test.tsx
+++ __tests__/packages.test.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import Packages from "../src/pages/packages";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("Package Screen testing", () => {
   it("should render the add packages button", () => {
@@ -10,5 +13,15 @@ describe("Package Screen testing", () => {
   it("should render the search bar", () => {
     render(<Packages />);
     expect(screen.getByTestId("package-search-bar")).toBeInTheDocument();
+  });
+  it("the add package button should be clickable", async () => {
+    render(<Packages />);
+    await userEvent.click(screen.getByTestId("add-package-btn"));
+    expect(screen.getByTestId("add-package-modal")).toBeInTheDocument();
+  });
+  it("search bar should hold the value that is typed in", () => {
+    render(<Packages />);
+    userEvent.type(screen.getByTestId("package-search-input"), "test");
+    expect(screen.getByTestId("package-search-input")).toHaveValue("test");
   });
 });
