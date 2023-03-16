7dbe4b50a7ab4903ce13edbf9d60bb1da40c6ff2
connorjbarry <barry31@purdue.edu>
2023-03-16 18:19:13 -0500

    navbar tells what page on

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "next/link";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 const NavBar = () => {
-  //   const router = useRouter();
+  const router = useRouter();
+
   return (
     <div className="sticky z-[100] h-20 w-full" data-testid="nav">
       <div className="flex h-full w-full items-center justify-between px-2 2xl:px-16">
@@ -17,18 +18,38 @@ const NavBar = () => {
         <div>
           <ul className="md:flex">
             <Link href="/" data-testid="nav-link">
-              <li className="ml-10 text-sm uppercase hover:underline">Home</li>
+              <li
+                className={`ml-10 text-sm uppercase hover:underline ${
+                  router.asPath === "/" ? "text-blue-600" : ""
+                }`}
+              >
+                Home
+              </li>
             </Link>
             <Link href="/packages" data-testid="nav-link">
-              <li className="ml-10 text-sm uppercase hover:underline">
+              <li
+                className={`ml-10 text-sm uppercase hover:underline ${
+                  router.asPath === "/packages" ? "text-blue-600" : ""
+                }`}
+              >
                 Packages
               </li>
             </Link>
-            <Link href="/" data-testid="nav-link">
-              <li className="ml-10 text-sm uppercase hover:underline">About</li>
+            <Link href="/about" data-testid="nav-link">
+              <li
+                className={`ml-10 text-sm uppercase hover:underline ${
+                  router.asPath === "/about" ? "text-blue-600" : ""
+                }`}
+              >
+                About
+              </li>
             </Link>
             <Link href="/account" data-testid="nav-link">
-              <li className="ml-10 text-sm uppercase hover:underline">
+              <li
+                className={`ml-10 text-sm uppercase hover:underline ${
+                  router.asPath === "/account" ? "text-blue-600" : ""
+                }`}
+              >
                 Account
               </li>
             </Link>
