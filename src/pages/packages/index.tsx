fd8b68eb826345caa80cc9b1d45b4df778356a70
connorjbarry <barry31@purdue.edu>
2023-03-22 17:19:14 -0400

    adding search filtering

--- src/pages/packages/index.tsx
+++ src/pages/packages/index.tsx
@@ -1,16 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
 import SearchBar from "../../components/SearchBar";
 import { Button, ButtonVariant } from "../../components/Button/Button";
 import AddPackageModal from "../../components/AddPackageModal";
 import PackagesTable from "~/components/Table/PackagesTable";
 import { api } from "~/utils/api";
+import { type IndivPkg } from "@prisma/client";
 
 const Packages = () => {
   const [searchInput, setSearchInput] = useState<string>("");
   const [showAddPackageModal, setShowAddPackageModal] =
     useState<boolean>(false);
+  let filteredPkgs: any;
 
   const allPkgs = api.packages.getAll.useQuery();
+  if (allPkgs.isLoading)
+    return <div className="flex items-center justify-center">Loading...</div>;
+  if (searchInput !== "") {
+    filteredPkgs = allPkgs.data?.filter(
+      (pkg) =>
+        pkg.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        pkg.author?.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    if ((filteredPkgs as IndivPkg[]).length === 0) filteredPkgs = null;
+  }
 
   return (
     <>
@@ -41,7 +54,11 @@ const Packages = () => {
       </div>
       <div className="mt-6 p-4">
         {allPkgs.data ? (
-          <PackagesTable pkgs={allPkgs.data} />
+          filteredPkgs && filteredPkgs !== null ? (
+            <PackagesTable pkgs={filteredPkgs as IndivPkg[]} />
+          ) : (
+            <PackagesTable pkgs={allPkgs.data} />
+          )
         ) : (
           <div className="mt-6 flex items-center justify-center">
             There are no packages in the registry
