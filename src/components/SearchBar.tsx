e77df59866f6dba8bb6e899b65063a2aee2c1300
connorjbarry <barry31@purdue.edu>
2023-03-22 19:28:39 -0400

    remove search button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -34,12 +34,6 @@ const SearchBar = ({
           placeholder="Search Packages..."
           required
         />
-        {/* <button
-          type="submit"
-          className="absolute right-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
-        >
-          Search
-        </button> */}
       </div>
     </form>
   );
